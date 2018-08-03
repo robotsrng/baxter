f08010ce947c317084c8f6bc09561ca9c75f5392
srng <shaneryanglass88@gmail.com>
2018-08-03 17:13:47 +1000

    Fixed my last commit.
    Works right up to the trim stage, I need to revamp the trim page and use
    javascript to activate all the trimming options rather than forms.

---
+++ src/main/resources/static/js/baxter.js
@@ -0,0 +1,9 @@
+function showVal(outputBox, newVal){
+	document.getElementById(outputBox).innerHTML=newVal;
+}
+function showValStart(outputBox, newVal){
+	document.getElementById(outputBox).innerHTML=newVal;
+}
+function showValEnd(outputBox, newVal){
+	document.getElementById(outputBox).innerHTML=newVal;
+}
